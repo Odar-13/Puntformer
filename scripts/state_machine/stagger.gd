31b132766b3f2f13840da02f9da784d4fb593c48
Bailey S <baileyps03@gmail.com>
2024-10-10 18:20:45 -0500

    Added state machine
    Needs animation flip on jump

---
+++ scripts/state_machine/stagger.gd
@@ -0,0 +1,29 @@
+extends PlayerState
+
+
+# Called when the node enters the scene tree for the first time.
+func enter(_msg := {}) -> void:
+	print('ENTER STAGGER ',player.velocity,_msg)
+
+
+func physics_update(_delta: float) -> void:
+	player.get_node("AnimatedSprite2D").frame=0
+	player.get_node("AnimatedSprite2D").stop()
+	
+	var stagger_dir_x = 1
+	var stagger_dir_y = 1
+	if player.velocity.x < 0:
+		stagger_dir_x = -1
+	var staggerVelocity = Vector2(-500 * stagger_dir_x, -90 * stagger_dir_y)
+	
+	player.velocity = Vector2.ZERO
+	player.knockback = staggerVelocity
+	player.move_and_slide()
+	
+	var iframe_time = player.get_node("AnimatedSprite2D/SpriteDMG/Timer").wait_time
+	player.get_node("PlayerStats").set_invunerable_for_seconds(iframe_time, true)
+	
+	player.staggerTween = get_tree().create_tween()
+	player.staggerTween.paralell().tween_property(player, "knockback",Vector2.ZERO, iframe_time)
+	
+	state_machine.transition_to("Air")
