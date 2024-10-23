a80e7b811ca0a2a952a75275edfd06206a366a8b
Vidalian <lrw9my@umsystem.edu>
2024-10-23 15:48:26 -0500

    detailed base enemy node

---
+++ enemy.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@export var health = 20
+@export var defense = 5
+@export var speed = 5
+@export var xp = 4
+@export var money = 8
+@export var consumable_item = "Algul"
+@export var gear_item = "The one ring"
+@export var item_pool = [consumable_item, gear_item]
+
+
+# Attacks assume the enemy is in position to attack. 
+
+func Attack1():
+	# Conduct attack, play appropriate animation
+	pass
+	
+func Attack2():
+	# Conduct attack, play appropriate animation
+	pass
