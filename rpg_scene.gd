8a6f83d92cb79c5b5537845d17adea7fda5e6fbf
Vidalian <lrw9my@umsystem.edu>
2024-10-27 00:46:13 -0500

    Background changer functional
    Needs both the actual backgrounds, and to be plugged in with the dictionary. After that, its good to go!

--- rpg_scene.gd
+++ rpg_scene.gd
@@ -1,13 +1,29 @@
 extends Node2D
 
-var battle_scene_background = "res://art/Fm1CIcZaYAMv96e.jpg"
-
-
 func _ready():
-	var background = get_node("../Background")
-	background.change_background(battle_scene_background)
+	#TODO: ONCE RECEIVE THE DICTIONARY FROM STATE TRANSITION, LOAD BACKGROUND
+	#_set_background(dictionary whatever)
 	pass
-	
+
+# Give the function the value of the "area" key in the dictionary, sets the appropriate background 
+func _set_background(area: String):
+	var background = get_node("Background")
+	if area == "dungeon":
+		#TODO: CREATE DUNGEON BACKGROUND
+		var battle_scene_background = ""
+		background.change_background(battle_scene_background)
+	elif area == "forest":
+		#TODO: CREATE FOREST BACKGROUND
+		var battle_scene_background = ""
+		background.change_background(battle_scene_background)
+	elif area == "mountain":
+		#TODO: CREATE MOUNTAIN BACKGROUND
+		var battle_scene_background = ""
+		background.change_background(battle_scene_background)
+	else:
+		var battle_scene_background = "res://art/placeholder rpg.jpg"
+		background.change_background(battle_scene_background)
+
 #Unsure what I want yet, so heres some planning
 """
 
