8d4b555bd2ddfd07d75867f86bc12c4265ece8e0
Vidalian <lrw9my@umsystem.edu>
2024-11-19 17:00:24 -0600

    Added selectable actions to new menu
    Also removed the old menu, I'll have to add controller support later but getting in the actual commands is way more important

--- scripts/RPG/menu_test.gd
+++ scripts/RPG/menu_test.gd
@@ -1,21 +1,28 @@
 extends VBoxContainer
 
-@export var has_button_2: bool
+@export var can_jump: bool = true # Should never be disabled
+@export var can_sword: bool = true # False until unlocked
+@export var can_magic: bool = true # False until atleast one magic is unlocked
+@export var can_item: bool = true # Generally always on, unless theres a boss that denies items
+@export var can_flee: bool = true # Always on, except during bosses
 
-var new_button
-var newer_button
-var newest_button
+var Actions = [
+	{"enabled": can_jump, "text": "Jump"},
+	{"enabled": can_sword, "text": "Sword"},
+	{"enabled": can_magic, "text": "Magic"},
+	{"enabled": can_item, "text": "Item"},
+	{"enabled": can_flee, "text": "Flee"}
+]
 
 func _ready() -> void:
-	var new_button = Button.new()
-	new_button.text = "Button created!"
-	add_child(new_button)
-	
-	if (has_button_2):
-		var newer_button = Button.new()
-		newer_button.text = "The COOLER button."
-		add_child(newer_button)
-		
-	var newest_button = Button.new()
-	newest_button.text = "Cringe"
-	add_child(newest_button)
+	position.x -= 250
+	for action in Actions:
+		if action["enabled"]:
+			var button = Button.new()
+			button.text = action["text"]
+			add_child(button)
+			button.connect("pressed", func(): _on_button_pressed(action["text"]))
+
+
+func _on_button_pressed(button_text):
+	print("Button '%s' pressed!" % button_text)
