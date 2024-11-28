b5456e82ea456e02117192270f0e297179201902
Brenn515 <bdc69c@umsystem.edu>
2024-11-28 13:27:18 -0600

    Improved the slide fix

--- scripts/state_machine/slide.gd
+++ scripts/state_machine/slide.gd
@@ -7,14 +7,18 @@ var slide_ready: bool = true
 var slide_speed = 1600
 var top_detected = false
 var bottom_detected = false
+var constant_size = Vector2(0,0)
+var y_scalar = 36
 
 #input needs to be constant or player can't move while sliding
 var constant_input = Vector2(0,0)
 
 func enter(_msg := {}) -> void:
+	constant_size = player.get_node("CollisionShape2D").shape.extents
 	player.get_node("AnimatedSprite2D").position = Vector2(0, 22)
-	player.get_node("CollisionShape2D").scale = Vector2(.25, .25)
-	player.get_node("CollisionShape2D").position = Vector2(0, 22)
+	#player.get_node("CollisionShape2D").scale = Vector2(.25, .25)
+	player.get_node("CollisionShape2D").shape.extents = Vector2(55.5,30)
+	player.get_node("CollisionShape2D").position = Vector2(0, y_scalar)
 	get_node("../../SlideArea/SlideHelp").disabled = false
 	get_node("../../SlideArea2/SlideHelp2").disabled = false
 	get_node("../../SlideCollision").disabled = false
@@ -46,7 +50,8 @@ func _on_slide_duration_timeout() -> void:
 	player.get_node("SlideCollision").disabled = true
 	player.get_node("AnimatedSprite2D").position = player.get_node("AnimatedSprite2D").position - Vector2(0, 22)
 	player.get_node("CollisionShape2D").scale = Vector2(1,1)
-	player.get_node("CollisionShape2D").position = player.get_node("CollisionShape2D").position - Vector2(0,22)
+	player.get_node("CollisionShape2D").shape.extents = constant_size
+	player.get_node("CollisionShape2D").position = player.get_node("CollisionShape2D").position - Vector2(0,y_scalar)
 	
 	print("Slide Duration timer stopped")
 	if get_parent().state== get_parent().get_node("RPG"):
